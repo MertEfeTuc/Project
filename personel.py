daddd5965250a6e2d73d28852f100a3e5e8ff298
Necati Mert Efe Tuç <mmertefe123@gmail.com>
2024-06-05 19:08:31 +0300

    personel sınıfı eklemesi

---
+++ personel.py
@@ -0,0 +1,37 @@
+
+
+class Personel:
+    def __init__(self,personel_no,ad,soyad,departman,maas):
+        self.__personal_no = personel_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__departman = departman
+        self.__maas = maas
+    def __str__(self):
+        return f"{self.__personal_no},{self.__ad},{self.__soyad},{self.__departman},{self.__maas}"
+    def no_set(self,no):
+        self.__personal_no = no
+    def ad_set(self,ad):
+        self.__ad = ad
+    def soyad_set(self,soyad):
+        self.__soyad = soyad
+    def dep_set(self,depart):
+        self.__departman = depart
+    def maas_set(self,maas):
+        self.__maas = maas
+
+    def no_get(self):
+        return self.__personal_no
+    def ad_get(self):
+        return self.__ad
+    def soyad_get(self):
+        return self.__soyad
+    def dep_get(self):
+        return self.__departman
+    def maas_get(self):
+        return self.__maas
+
+
+newobject = Personel(20,"ali","baba","HR",50000)
+
+
